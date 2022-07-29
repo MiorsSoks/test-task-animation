381ea885da44ad40f43c4f886b2787b4322636a3
Myroslav Sokolov <mirossoks@gmail.com>
2022-07-29 16:14:00 +0300

    add stop animation

--- index.js
+++ index.js
@@ -4,33 +4,25 @@ const firstBarrel = document.querySelector(".barrel-first");
 const secondBarrel = document.querySelector(".barrel-second");
 const thirdBarrel = document.querySelector(".barrel-third");
 
+
 function handleClick(event) {
-  var loopBegan = 0;
-var loopCompleted = 5;
+
   firstBarrel.classList.toggle("loop-first");
   secondBarrel.classList.toggle("loop-second");
   thirdBarrel.classList.toggle("loop-third");
-anime({
+  anime({
   targets: '.loop-first',
   translateX: 270,
   direction: 'alternate',
-  loop: true,
+  loop: 2,
   easing: 'easeInOutSine',
-  loopBegin: function(anim) {
-    loopBegan++;
-    beginLogEl.value = 'loop began : ' + loopBegan;
-  },
-  loopComplete: function(anim) {
-    loopCompleted++;
-    completeLogEl.value = 'loop completed : ' + loopCompleted;
-  }
   
 });
   anime({
   targets: '.loop-second',
     translateX: 200,
   direction: 'alternate',
-    loop: true,
+    loop: 2,
   easing: 'easeInOutSine',
     delay: 100,
     endDelay: 100,
@@ -40,15 +32,20 @@ anime({
   targets: '.loop-third',
   translateX: -350,
   direction: 'alternate',
-  loop: true,
+  loop: 2,
       easing: 'easeInOutSine',
+          delay: 20,
+    endDelay: 20,
   
-});
+    });
 }
 
 const addSelector = (event) => {
   event.target.classList.toggle("barrel-up");
-  anime({
+  firstBarrel.classList.toggle("loop-first");
+  secondBarrel.classList.toggle("loop-second");
+  thirdBarrel.classList.toggle("loop-third");
+  var first = anime({
     targets: ".barrel-up",
     translateY: -60,
     duration: 1200,
